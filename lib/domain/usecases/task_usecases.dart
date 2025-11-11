85cd4b70ef4dddb96e15b4f4035a61727435b8ff
VitorFranciscoDev <vitorfndr@gmail.com>
2025-11-11 10:30:09 -0300

    feat: task use cases

--- lib/domain/usecases/task_usecases.dart
+++ lib/domain/usecases/task_usecases.dart
@@ -1,3 +1,40 @@
+import 'package:to_do_list/domain/entities/task.dart';
+import 'package:to_do_list/domain/repositories/task_repository.dart';
+
 class TaskUseCases {
-  
+  const TaskUseCases({ required this.repository });
+
+  final TaskRepository repository;
+
+  Future<int> addTask(Task task) async {
+    try {
+      return await repository.addTask(task);
+    } catch(e) {
+      throw Exception("Error in Add Task Task Use Cases: $e");
+    }
+  }
+
+  Future<int> deleteTask(int? id) async {
+    try {
+      return await repository.deleteTask(id);
+    } catch(e) {
+      throw Exception("Error in Delete Task Task Use Cases: $e");
+    }
+  }
+
+  Future<int> updateTask(Task task) async {
+    try {
+      return await repository.updateTask(task);
+    } catch(e) {
+      throw Exception("Error in Update Task Task Use Cases: $e");
+    }
+  }
+
+  Future<List<Task>> getAllTasks(int? uid) async {
+    try {
+      return await repository.getAllTasks(uid);
+    } catch(e) {
+      throw Exception("Error in Get All Tasks Task Use Cases: $e");
+    }
+  }
 }
